b4388a561cba435cd3480e09ee01c8d0ae5e6788
JKF <doublejaygithub@gmail.com>
2021-11-29 17:29:00 +0200

    UI UserCard

--- src/sample/Controllers/Items/UserCard.java
+++ src/sample/Controllers/Items/UserCard.java
@@ -1,4 +1,21 @@
 package sample.Controllers.Items;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class UserCard {
+
+    @FXML
+    Label usernameLabel, idLabel, emailLabel, friendsNumberLabel;
+
+    public void setLabels(String name, String email, int id) {
+        usernameLabel.setText(name);
+        emailLabel.setText(email);
+        idLabel.setText((String.valueOf(id)));
+    }
+
+    public void addFriend() {
+
+    }
+
 }
--- src/sample/Controllers/ViewOtherReaders.java
+++ src/sample/Controllers/ViewOtherReaders.java
@@ -8,8 +8,10 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import sample.Controllers.Items.UserCard;
 import sample.DataStructures.Stack;
 import sample.Main;
+import sample.Models.User;
 
 import java.io.IOException;
 
@@ -21,18 +23,21 @@ public class ViewOtherReaders {
     HBox userCardBox;
 
     @FXML
-    public void initialize() throws  IOException{
+    public void initialize() throws IOException {
         Main.restoreSize();
-        for(int i =0 ; i< Main.userList.size() ; i++){
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/View/Items/UserCard.fxml"));
-            Parent userCard= loader.load();
+        for (int i = 0; i < Main.userList.size(); i++) {
+            User u = Main.userList.get(i);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/View/Items/UserCardTest.fxml"));
+            Parent userCard = loader.load();
+            UserCard userCard1 = loader.getController();
+            userCard1.setLabels(u.getName(), u.getEmail(), u.getID());
             userCardBox.getChildren().add(userCard);
         }
     }
 
     public void searchItem() throws Exception {
         System.out.println(searchField.getText());
-        if(searchField.getText().length() > 0){
+        if (searchField.getText().length() > 0) {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/View/Search.fxml"));
             Parent page = loader.load();
             Main.switchScene(page);
@@ -40,8 +45,8 @@ public class ViewOtherReaders {
     }
 
     public void navigate(ActionEvent event) throws IOException {
-        Button b1= (Button) event.getTarget();
-        try{
+        Button b1 = (Button) event.getTarget();
+        try {
             if ("addBook".equals(b1.getId())) {
                 FXMLLoader page = new FXMLLoader(getClass().getResource("/sample/View/AddBook.fxml"));
                 Parent root = page.load();
@@ -53,16 +58,16 @@ public class ViewOtherReaders {
                 Parent root1 = page2.load();
                 Main.switchScene(root1);
 
-            }else if("ViewLatestAdditions".equals(b1.getId())){
+            } else if ("ViewLatestAdditions".equals(b1.getId())) {
                 FXMLLoader page2 = new FXMLLoader(getClass().getResource("/sample/View/ViewLatestAdditions.fxml"));
                 Parent root1 = page2.load();
                 Main.switchScene(root1);
-            }else if("ViewOtherReaders".equals(b1.getId())){
+            } else if ("ViewOtherReaders".equals(b1.getId())) {
                 FXMLLoader page2 = new FXMLLoader(getClass().getResource("/sample/View/ViewOtherReaders.fxml"));
                 Parent root1 = page2.load();
                 Main.switchScene(root1);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("something went wrong");
         }
 
